a1f6293134cd681492fe25b8dae4eb8f0bf5246f
looooo <sppedflyer@gmail.com>
2017-09-15 14:31:41 +0200

    CMake: - install to a std. python install directory - don't add directories to sys.path, instead use a default location to place the python library - use -std=c++1y instead of -std=c++14 - don't install occvis if USE_GUI=OFF - additional conditionals for USE_GUI=OFF - install nglib.h

--- python/__init__.py
+++ python/__init__.py
@@ -6,10 +6,10 @@ import tkinter
 _netgen_bin_dir=os.path.realpath(os.path.join(os.path.dirname(__file__),'..','@NETGEN_PYTHON_RPATH_BIN@'))
 _netgen_lib_dir=os.path.realpath(os.path.join(os.path.dirname(__file__),'..','@NETGEN_PYTHON_RPATH@'))
 
-if sys.platform.startswith('win'):
-    os.environ['PATH'] += ';'+os.path.realpath(os.path.join(os.path.dirname(__file__),'../../../bin'))
+# from . import libngpy
+# del path
 
-del sys
-del os
+# del sys
+# del os
 
 from . import libngpy
